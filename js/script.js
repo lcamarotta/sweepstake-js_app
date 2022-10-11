9a6bcfef4894461606bb8794198b9a0bb6504465
Lucas Camarotta <lucas_camarotta@hotmail.com>
2022-10-11 02:04:38 -0300

    feature: add 'Show Winners' / 'Clear Winners' buttons

--- js/script.js
+++ js/script.js
@@ -7,20 +7,23 @@ class person {
     this.email = email;
     this.isWinner= false;
   }
+  winnerFlagClear(){
+    this.isWinner = false;
+  }
 }
 
 let peopleList = []
-let peopleCounter = document.getElementById("peopleCounter")
+const peopleCounter = document.getElementById("peopleCounter")
 
 //register button
-let enroll = document.getElementById("enroll-btn")
+const enroll = document.getElementById("enroll-btn")
 enroll.onclick = (e) => {
   e.preventDefault()
-  let firstname = document.getElementById("inputFirstname")
-  let lastname = document.getElementById("inputLastname")
-  let age = document.getElementById("inputAge")
-  let phone = document.getElementById("inputPhone")
-  let email = document.getElementById("inputEmail")
+  const firstname = document.getElementById("inputFirstname")
+  const lastname = document.getElementById("inputLastname")
+  const age = document.getElementById("inputAge")
+  const phone = document.getElementById("inputPhone")
+  const email = document.getElementById("inputEmail")
 
   peopleList.push( new person(firstname.value, lastname.value, age.value, phone.value, email.value) )
   peopleCounter.innerText = `${peopleList.length}` //display enrolled people number
@@ -29,15 +32,28 @@ enroll.onclick = (e) => {
 //sweepstake button
 const pickWinner = document.getElementById("pickWinner-btn")
 pickWinner.onclick = () => {
-  const peopleList_noWinners = peopleList.filter( (i) => i.isWinner === false)  //filter out people who already won
+  let peopleList_noWinners = peopleList.filter( (i) => i.isWinner === false)  //filter out people who already won
   if (peopleList_noWinners.length == 0) alert('Todas las personas registradas ya ganaron al menos una vez. Se debe registrar mas personas o resetear los ganadores')
   let winner = getRandomInt(peopleList_noWinners.length)  //find winner based on: (math.random => array position)
   peopleList[winner].isWinner = true  //flag winner
   renderWinner(winner)  //display winner
 }
 
-//show winners
+//clear winners flag button
+const clearFlags = document.getElementById("clearFlags-btn")
+clearFlags.onclick = () => {
+  peopleList.forEach( person => { person.winnerFlagClear() }); //clear flags using object.metod
+  const winnerCardsDiv = document.getElementById("winnerCardsDiv")
+  winnerCardsDiv.innerHTML = ""
+}
+
+//show every winner
 const showWinner = document.getElementById("showWinner-btn")
+showWinner.onclick = () => {
+  let peopleList_Winners = peopleList.filter( (i) => i.isWinner === true)
+  if (peopleList_Winners.length == 0) alert('Aun no hay ningun ganador')
+  renderEveryWinner(peopleList_Winners)
+}
 
 //functions:
 function getRandomInt(maxNum) {
@@ -45,14 +61,36 @@ function getRandomInt(maxNum) {
 }
 
 function renderWinner(winner){
-  let winnerDiv = document.getElementById("winnerDiv")
+  const winnerDiv = document.getElementById("winnerDiv")
   winnerDiv.innerHTML = `
                           <div class="container setBorder">
                             <p class="titleBig mb-1">Saliste Sorteado!</p>
                             <p class="titleSmall mb-1">${peopleList[winner].firstname} ${peopleList[winner].lastname}</p>
                             <p class="my-0">Tel: ${peopleList[winner].phone}</p>
-                            <p class="my-0">email: ${peopleList[winner].email}</p>
+                            <p class="my-0">Email: ${peopleList[winner].email}</p>
                           </div>
                         `
   winnerDiv.className = "col my-5"
+}
+
+function renderEveryWinner(winners){
+  let num = 0
+  let card
+  const winnerCardsDiv = document.getElementById("winnerCardsDiv")
+  winnerCardsDiv.innerHTML = ""
+  winners.forEach(winner => {
+    num++
+    card = document.createElement("div")
+    card.classList.add('card', 'col-2', 'm-2')
+    card.innerHTML =  `
+                        <img src="./img/${num}.jpg" class="card-img-top" alt="Imagen de premio">
+                        <div class="card-body text-center">
+                          <h5 class="card-title">Premio Nro.${num}</h5>
+                          <p class="card-text my-0">${winner.firstname} ${winner.lastname}</p>
+                          <p class="card-text my-0">Tel: ${winner.phone}</p>
+                          <p class="card-text my-0">Email: ${winner.email}</p>
+                        </div>
+                      `
+    winnerCardsDiv.appendChild(card)
+  });
 }
